e88e127066f6c22a856614e1eb281830c4c5482d
wolfbolin <wolfbolin@foxmail.com>
2019-02-28 13:33:46 +0800

    2019-02-28
    移除对powershell的依赖
    增强对自定义路径的支持

--- BiliUtil/bili_album.py
+++ BiliUtil/bili_album.py
@@ -102,14 +102,15 @@ class Album:
         if len(self.video_list) == 0:
             self.get_album_info()
 
+        base_path = os.path.abspath(base_path)  # 获取绝对路径地址
         if name_path:
             # 检查路径名中的特殊字符
             temp_name = re.sub(r"[\/\\\:\*\?\"\<\>\|\s'‘’]", '_', self.name)
             if len(temp_name) == 0:
                 temp_name = self.aid
-            cache_path = base_path + './{}'.format(temp_name)
+            cache_path = base_path + '/{}'.format(temp_name)
         else:
-            cache_path = base_path + './{}'.format(self.aid)
+            cache_path = base_path + '/{}'.format(self.aid)
         if not os.path.exists(cache_path):
             os.makedirs(cache_path)
 
