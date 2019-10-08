a6d954bbe24206f6aa8ae7cc36a238b22083e8e6
Justlearnm0re <driverczn@gmail.com>
2019-10-08 11:44:28 +0800

    Modify Album's attr for downloading whole album.

---
+++ example_download_album.py
@@ -0,0 +1,14 @@
+import BiliUtil
+
+album = BiliUtil.Album(7134874)
+album_info = album.sync()
+print(album_info)
+video_list = album.get_video_list()
+# print(video_list)
+
+for video in video_list:
+    #print(video)
+    video.sync(cookie="SESSDATA=22c52c9...")
+    video_name = ("P%03d " % video.page) + album.page_list[video.page - 1]
+    task = BiliUtil.Task(video, 'D:/BiliUtil', video_name)
+    task.start()
